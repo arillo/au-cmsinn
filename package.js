8e30729e16e691a412086b5369d8c4dc1311cb08
banglashi <dm@arillo.net>
2014-12-09 21:58:58 +0100

    trying hallojs

--- package.js
+++ package.js
@@ -16,7 +16,7 @@ Package.on_use(function (api, where) {
     api.imply(['alanning:roles']);
 
     api.use(['underscore@1.0.0', 'ui@1.0.2', 'iron:router@1.0.0', 'tracker@1.0.2', 'templating@1.0.6', 'alanning:roles@1.2.12', 'meteorhacks:subs-manager@1.2.2'], ['client', 'server']);
-    api.use(['jquery@1.0.0', 'mrt:jquery-ui@1.9.2', 'arillo:meteor-x-editable@1.5.2-1'], ['client']);
+    api.use(['jquery@1.0.0', 'mrt:jquery-ui@1.9.2', 'arillo:meteor-x-editable@1.5.2-1','matteodem:hallo@1.0.4'], ['client']);
 
     api.add_files('lib/storage/remote-collection-storage.js', ['client', 'server']);
     api.add_files('lib/plugins/core.js', ['client', 'server']);
